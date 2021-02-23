5c484beed342f41108318ead3f702d25a221b26f
razr1 <284512180@qq.com>
2021-02-23 22:26:29 +0800

    组件测试

--- src/exports/TestView/index.tsx
+++ src/exports/TestView/index.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
-
+import {Button, Modal} from "antd"
 interface TestViewProps{
   label:string
 }
 
 export default class TestView extends React.Component<TestViewProps>{
+  
+  state = {
+    visible:false
+  }
+
+  onShow = (visible:boolean)=>{
+    this.setState({
+      visible
+    })
+  }
+
   render(){
     const {label} = this.props;
-    return <div>TestView---{label}</div>
+    const {visible} = this.state;
+    return <div><Button onClick={()=>{this.onShow(true)}}>show modal</Button>---{label}<Modal title="antd modal" visible={visible} onCancel={()=>{this.onShow(false)}}>xxxxxxxxxxxx</Modal></div>
   }
 }
