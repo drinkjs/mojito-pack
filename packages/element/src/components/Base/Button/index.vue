e8070a8373f3f592852d05bd2206b76917e4db0f
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2023-08-04 18:35:51 +0800

    优化包结构

--- packages/element/src/components/Base/Button/index.vue
+++ packages/element/src/components/Base/Button/index.vue
@@ -34,6 +34,11 @@ export const PackButton = CreatePack(Button, {
       type:"object",
       description:"https://element-plus.org/zh-CN/component/button.html#button-attributes",
     }
+  },
+  events:{
+    onClick:{
+      name:"点击",
+    }
   } 
 });
 
